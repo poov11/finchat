f8bacdf27d324e13a782d6b96a86faf3b9b5c908
poov11 <143461350+poov11@users.noreply.github.com>
2023-11-19 14:47:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+
+<?php include_once "header.php"; ?>
+<?php include_once "index.html"; ?>
